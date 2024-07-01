b405e715ca4b4d7e8ae34aae59f2d56ad41a0efa
Vaisalee Balakrishnan <vaisalee04@gmail.com>
2024-07-01 15:38:11 +0530

    1st part completed - backend

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from 'cookie-parser';
+
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js"
+
+import connectToMongoDB from './db/connectToMongoDB.js';
+
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+dotenv.config();
+
+app.use(express.json()); // to parse the incoming requests with JSON payloads (fromm req.body)
+app.use(cookieParser()); // to parser the incoming cookies from req.cookies
+
+
+app.use("/api/auth",authRoutes);
+app.use("/api/messages",messageRoutes);
+app.use("/api/users", userRoutes);
+
+
+
+
+
+app.listen(PORT, () => {
+    connectToMongoDB();
+    console.log(`Server Running on port ${PORT}`);
+});
