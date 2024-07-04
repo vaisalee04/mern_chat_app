347b8fd885b5405f73d48bbbe38162432996c70d
Vaisalee Balakrishnan <vaisalee04@gmail.com>
2024-07-04 14:47:44 +0530

    DEPLOYED

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/home/Home";
-import Login from "./pages/login/login.jsx";
+import Login from "./pages/login/login";
 import SignUp from "./pages/signup/SignUp";
 import { Toaster } from "react-hot-toast";
 import { useAuthContext } from "./context/AuthContext";
@@ -11,8 +11,8 @@ function App() {
 	return (
 		<div className='p-4 h-screen flex items-center justify-center'>
 			<Routes>
-				<Route path='/' element={authUser ? <Home /> : <Navigate to={"/Login"} />} />
-				<Route path='/Login' element={authUser ? <Navigate to='/' /> : <Login />} />
+				<Route path='/' element={authUser ? <Home /> : <Navigate to={"/login"} />} />
+				<Route path='/login' element={authUser ? <Navigate to='/' /> : <Login />} />
 				<Route path='/signup' element={authUser ? <Navigate to='/' /> : <SignUp />} />
 			</Routes>
 			<Toaster />
